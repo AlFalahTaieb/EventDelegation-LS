7cb8ccec50b4544ad5968bba99e952ae387663c2
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-11 16:08:11 +0200

    LS working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
-  const items = [];
+  const items = JSON.parse(localStorage.getItem('items')) || [];
 
   function addItem (e) {
   e.preventDefault();
@@ -12,7 +12,8 @@
 
 
   items.push(item)
-  pList(items,itemsList)
+  pList(items,itemsList);
+  localStorage.setItem('items',JSON.stringify(items));
   this.reset();
   }
 
@@ -20,7 +21,8 @@ function pList (plates=[],platesList) {
 	platesList.innerHTML =  plates.map((plate,i)=>{
 		return`
 		<li>
-		<label for="">${plate.text}</label>
+          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+          <label for="item${i}">${plate.text}</label>
 		</li>
 
 		`;
@@ -28,4 +30,15 @@ function pList (plates=[],platesList) {
 
 }
 
+
+ function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    pList(items, itemsList);
+  }
   addItems.addEventListener('submit',addItem)
+  itemsList.addEventListener('click', toggleDone);
+ pList(items,itemsList);
